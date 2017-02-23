cf606bf1e004d6c8e40d31dc94f639896aaee7ef
Prof Phillips <profphillips@gmail.com>
2017-02-23 13:49:42 +0000

    Updated C13N17 to support cloneable.

--- src/week4/C13N17.java
+++ src/week4/C13N17.java
@@ -6,9 +6,17 @@ package week4;
  * @author John Phillips
  */
 public class C13N17 {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
         Complex c1 = new Complex(3.5, 5.5);
-        
+        Complex c2 = (Complex) c1.clone();
+        System.out.println("c1=" + c1);
+        System.out.println("c2=" + c2);
+        c2.setA(5.5);
+        c2.setB(8.5);
+        System.out.println("c1=" + c1);
+        System.out.println("c2=" + c2);
+        System.out.println("c2-c1="+c2.sub(c1));
     }
 }
 
@@ -40,11 +48,20 @@ class Complex implements Cloneable {
     }
 
     public Complex add(Complex c) {
-        return new Complex((a+c.getA()),(b+c.getB()));
+        return new Complex((a + c.getA()), (b + c.getB()));
     }
-    
+
     public Complex sub(Complex c) {
-        return new Complex((a-c.getA()), (b-c.getB()));
+        return new Complex((a - c.getA()), (b - c.getB()));
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "Complex{" + "a=" + a + ", b=" + b + '}';
     }
 
 }
